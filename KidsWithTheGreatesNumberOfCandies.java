c534ce99c64be0704275cc01e04e2312b8ff35ac
Varun Gollapalli <gvarun1999@gmail.com>
2021-03-13 22:53:05 +0530

    Update KidsWithTheGreatesNumberOfCandies.java

--- KidsWithTheGreatesNumberOfCandies.java
+++ KidsWithTheGreatesNumberOfCandies.java
@@ -30,3 +30,32 @@ class Solution {
 		return ans;
 	}
 }
+
+/* better solution: 0ms runtime
+  public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+		
+
+		List<Boolean> ans = new ArrayList<>();
+		int max = candies[0];
+       for(int i =0;i<candies.length;i++)
+       {
+           if(candies[i]>max)
+           {
+               max=candies[i];
+           }
+       }
+       
+       for(int i =0;i<candies.length;i++)
+       {
+           if(candies[i]+extraCandies>=max)
+           {
+               ans.add(true);
+           }
+           else
+           {
+               ans.add(false);
+           }
+       }
+       return ans;
+	}
+} */
