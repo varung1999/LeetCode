8287f9437fd233c3213a0ace190bb697648e0cc7
Varun Gollapalli <gvarun1999@gmail.com>
2021-03-15 23:04:00 +0530

    Create HowManyNumbersAreSmallerThanCurrentNumber.java

---
+++ HowManyNumbersAreSmallerThanCurrentNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+   public static int[] smallerNumbersThanCurrent(int[] nums) {
+	     int ans[]=new int[nums.length];  
+	     int count = 0;
+		 for(int i =0;i<nums.length;i++)
+	        {	
+	        	for(int j = 0;j<nums.length;j++)
+	        	{
+	        		if(nums[i]>nums[j])
+	        		{
+	        			count++;
+	        		}
+	        		
+	        	}
+	        	ans[i]=count;
+	        	count=0;
+	        	
+	        	
+	        }
+		 return ans;
+	    }
+}
