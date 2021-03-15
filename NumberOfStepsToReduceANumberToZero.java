6a27e1c39c7d29d09858a3e46c38d890b6930964
Varun Gollapalli <gvarun1999@gmail.com>
2021-03-15 23:03:09 +0530

    Create NumberOfStepsToReduceANumberToZero.java

---
+++ NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int numberOfSteps (int num) {
+        int count = 0;
+        
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num=num/2;
+                count++;
+            }
+            else
+            {
+                num=num-1;
+                count++;
+            }
+        }
+        return count;
+    }
+}
