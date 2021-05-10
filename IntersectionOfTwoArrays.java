0a45ab9171fb0d6355962238effd8468211bd71e
Varun Gollapalli <gvarun1999@gmail.com>
2021-05-10 23:48:07 +0530

    Update IntersectionOfTwoArrays.java
    Problem Statement: https://leetcode.com/problems/intersection-of-two-arrays/submissions/

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -1,34 +1,30 @@
-import java.util.*;
 class Solution {
    public static int[] intersection(int[] nums1, int[] nums2) {
 		
-		Arrays.sort(nums1);
-		Arrays.sort(nums2);
-		ArrayList<Integer> ans = new ArrayList<>();
-		for(int i =0;i<nums2.length;i++)
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int i:nums1)
 		{
-			for(int j=0;j<nums1.length;j++ )
+			set.add(i);
+		}
+		
+		HashSet<Integer> set2 = new HashSet<>();
+		for(int i:nums2)
+		{
+			if(set.contains(i))
 			{
-				if(nums2[i]==nums1[j] && isPresent(ans,nums2[i])==false)
-				{
-					ans.add(nums2[i]);
-				}
+				set2.add(i);
 			}
 		}
 		
-         int[] arr = ans.stream().mapToInt(i -> i).toArray();
-       return arr;
-	}
-	
-	public static boolean isPresent(ArrayList<Integer> nums, int key)
-	{
-		for(int i =0;i<nums.size();i++)
+		int[] result = new int[set2.size()];
+		int index= 0;
+		for(int i:set2)
 		{
-			if(nums.get(i)==key)
-			{
-				return true;
-			}
+			result[index++]=i;
 		}
-		return false;
+		
+		return result;
 	}
+
 }
