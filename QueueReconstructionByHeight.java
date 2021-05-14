4a44d78f1223d448a2d5f7c4ab3ba0aa815a674d
Varun Gollapalli <gvarun1999@gmail.com>
2021-05-14 18:21:28 +0530

    Update QueueReconstructionByHeight.java

--- QueueReconstructionByHeight.java
+++ QueueReconstructionByHeight.java
@@ -1,5 +1,10 @@
-class Solution {
-    public  int[][] reconstructQueue(int[][] people) {
+package practice;
+
+import java.util.*;
+
+public class QueueReconstructionbyHeight {
+
+	public  int[][] reconstructQueue(int[][] people) {
 
 		Arrays.sort(people,new peopleComparator());
 		
@@ -14,11 +19,36 @@ class Solution {
 		return list.toArray(new int[people.length][2]);
 		
 	}
-    
-    public class peopleComparator implements Comparator<int[]>{
-        public int compare(int[] one,int[] two){
-            if(one[0]==two[0]) return one[1]-two[1];
-            return two[0]-one[0];
-        }
-    }
+	
+	public class peopleComparator implements Comparator<int[]> {
+		public int compare(int[] one,int[] two)
+		{
+			if(one[0]==two[0])
+			{
+				return one[1]-two[1];
+			}
+			return two[0]-one[0];
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[][] = {{9,0},{7,0},{1,9},{3,0},{2,7},{5,3},{6,0},{3,4},{6,2},{5,2}};
+		
+		QueueReconstructionbyHeight q = new QueueReconstructionbyHeight();
+		int ans[][] =q.reconstructQueue(arr);
+		
+				for(int i=0;i<ans.length;i++)
+				{
+					for(int j = 0;j<ans[0].length;j++)
+					{
+						System.out.print(ans[i][j]+" ");
+						
+					}
+					System.out.println();
+				}
+
+		
+
+	}
+
 }
