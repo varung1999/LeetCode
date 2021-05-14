05290e2564efa60898fc5f7afc0222e65b800770
Varun Gollapalli <gvarun1999@gmail.com>
2021-05-14 12:08:40 +0530

    Create PowerOfThree.java
    Problem Statement: https://leetcode.com/problems/power-of-three/

---
+++ PowerOfThree.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class PowerOfThree {
+
+	public static boolean isPowerOfThree(int n) {
+		
+		if(n<1)
+		{
+			return false;
+		}
+		
+		while(n%3==0)
+		{
+			n=n/3;
+		}
+		
+		return n==1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isPowerOfThree(1));
+		
+	}
+
+}
