f179bcfa523a71e64b5a0e53c21f833b0f36dd95
Varun Gollapalli <gvarun1999@gmail.com>
2021-09-01 02:22:59 -0700

    Create LengthOfLastWord.java

---
+++ LengthOfLastWord.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int count =0;
+		
+		int k=0;
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				k=i;
+				break;
+			}
+			
+		}
+		
+		
+		for(int i=k;i>=0;i--)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return count;  
+    }
+}
