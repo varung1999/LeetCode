7b8666d3307ef1f99172d3720f6da2a2345f0b43
Varun Gollapalli <gvarun1999@gmail.com>
2022-02-23 19:21:49 -0700

    Update HouseRobber.java

--- Dynamic Programming Study Plan/HouseRobber.java
+++ Dynamic Programming Study Plan/HouseRobber.java
@@ -1,17 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
         
-        int n=nums.length;
-        if(n==1)
-            return nums[0];       
-        int[]dp=new int[n];       
+        if(nums.length==0)
+        {
+            return 0;
+        }
+        
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+        
+        if(nums.length==2)
+        {
+            return Math.max(nums[0],nums[1]);
+        }
+        
+        int dp[] = new int[nums.length];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
-        int max=dp[0];
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-1],max+nums[i]);
-            max=Math.max(dp[i-1],max);
+        
+        for(int i = 2;i<nums.length;i++)
+        {
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
         }
-        return dp[n-1];
+        
+        return dp[nums.length-1];
     }
 }
