878056a7f54563ce34378025dfbcf199a52cba90
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-11 21:39:41 -0700

    Create NumberOfIslands_BFS.java

---
+++ Graph Theory - 1/NumberOfIslands_BFS.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        
+        int total = 0;
+        Queue<int[]> q = new LinkedList<>(); 
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}}; 
+        for(int i = 0;i<grid.length;i++)
+        {
+            for(int j = 0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    q.add(new int[]{i,j});
+                    grid[i][j]='0';
+                    total++;
+                    
+                    while(!q.isEmpty())
+                    {
+                        int[] curr = q.poll();
+                        
+                        for(int[] dir:dirs)
+                        {
+                            int r = dir[0]+curr[0];
+                            int c = dir[1]+curr[1];
+                            
+                            
+                            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c] == '1')
+                            {
+                                grid[r][c] = '0';
+                                q.add(new int[]{r,c});
+                                
+                            }
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
+        
+        return total;
+    }
+    
+   
+}
