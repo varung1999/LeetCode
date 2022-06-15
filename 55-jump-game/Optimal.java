47c03d76bb0cb9f92776aa06b550206c29984d78
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-15 10:55:35 -0700

    Create Optimal.java

---
+++ 55-jump-game/Optimal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int last = nums.length - 1;
+        for(int i = last;i>=0;i--)
+        {
+            if(i+nums[i]>=last)
+            {
+                last = i;
+            }
+        }
+        
+        return last == 0;
+    }
+}
+
+
+/*
+
+  TC = O(N) 
+  SC = O(1)
+
+*/
