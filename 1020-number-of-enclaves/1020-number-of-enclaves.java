4fb331f52d7a89a1be5f124588f243292c51871a
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-16 12:39:13 -0700

    Time: 14 ms (19.44%), Space: 66.7 MB (18.45%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,64 +1,54 @@
 class Solution {
-    int total,count;
-    boolean isBorder;
     public int numEnclaves(int[][] grid) {
-        if(grid == null || grid.length == 0)
-        {
-            return 0;
-        }
+        if(grid == null || grid.length == 0) return 0;
         
-        total = 0;
-        isBorder = false;
+        
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs  = {{-1,0},{1,0},{0,-1},{0,1}};
         
         for(int i =0;i<grid.length;i++)
         {
             for(int j = 0;j<grid[0].length;j++)
             {
-                if(grid[i][j] == 1)
+                if((i==0 || i==grid.length-1 || j==0 || j==grid[0].length -1) && grid[i][j]==1)
                 {
-                    isBorder = true;
-                    count = 0;
-                    dfs(grid,i,j);
+                    q.add(new int[]{i,j});
+                    grid[i][j] = 0;
                     
-                    if(isBorder)
+                    while(!q.isEmpty())
                     {
-                        total+=count;
+                        int[] curr = q.poll();
+                        
+                        for(int[] dir: dirs)
+                        {
+                            int r = dir[0]+curr[0];
+                            int c = dir[1]+curr[1];
+                            
+                            
+                            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c] == 1)
+                            {
+                                q.add(new int[]{r,c});
+                                grid[r][c] = 0;
+                            }
+                        }
                     }
                 }
             }
         }
         
+        int total = 0;
+        
+        for(int i =0;i<grid.length;i++)
+        {
+            for(int j =0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    total++;
+                }
+            }
+        }
+        
         return total;
-    }
-    
-    private void dfs(int[][] grid, int r, int c)
-    {
-        //base
-        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]!=1)
-        {
-            return;
-        }
-        
-        if(borderHelper(grid,r,c))
-        {
-            isBorder = false;
-        }
-        
-        //logic
-        if(grid[r][c] == 1)
-        {
-            grid[r][c] = 0;
-                count++;
-        }
-        
-        dfs(grid,r-1,c);
-        dfs(grid,r+1,c);
-        dfs(grid,r,c-1);
-        dfs(grid,r,c+1);
-    }
-    
-    private boolean borderHelper(int[][] grid, int r, int c)
-    {
-        return r==0 || c==0 || r==grid.length-1 || c == grid[0].length - 1;
     }
 }
