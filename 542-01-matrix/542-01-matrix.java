8c56605b9b3eb88f9de10d81af1c03984deb0eaf
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-17 11:54:05 -0700

    Time: 688 ms (5.02%), Space: 70.8 MB (66.94%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,56 +1,149 @@
 class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        if(mat == null || mat.length == 0) return mat;
-        
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        for(int i =0;i<m;i++)
-        {
-            for(int j =0;j<n;j++)
-            {
-                if(mat[i][j] == 0)
-                {
-                    q.add(new int[]{i,j});
-                }
-                else
-                {
-                    mat[i][j] = -1;
-                }
-            }
-        }
-        
-        
-        int dist = 1;
-        while(!q.isEmpty())
-        {
-            int size = q.size();
-            
-            for(int i =0;i<size;i++)
-            {
-                int[] curr = q.poll();
-                
-                for(int[] dir: dirs)
-                {
-                    int r = curr[0] + dir[0];
-                    int c = curr[1] + dir[1];
-                    
-                    
-                    if(r>=0 && r<m &&c>=0 && c<n && mat[r][c] == -1)
-                    {
-                        
-                        mat[r][c] = dist;
-                        q.add(new int[]{r,c});
-                    }
-                }
-            }
-            dist++;
-        }
-        
-        return mat;
-    }
+
+
+
+private int m;
+
+
+
+private int n;
+
+
+
+public int[][] updateMatrix(int[][] matrix) {
+
+
+
+m = matrix.length;
+
+
+
+// edge case
+
+
+
+if(matrix == null || m == 0) return matrix;
+
+
+
+n= matrix[0].length;
+
+
+
+int [][] result = new int [m][n];
+
+
+
+for(int i = 0; i < m; i++){
+
+
+
+for(int j = 0; j < n; j++){
+
+
+
+result[i][j] = dfs(matrix,result, i, j);
+
+
+
+}
+
+
+
+}
+
+
+
+return result;
+
+
+
+}
+
+
+
+private int dfs(int[][] matrix, int [][] result,int i, int j){
+
+
+
+// base case
+
+
+
+if(i < 0 || i >= m || j < 0 || j >= n) return 9999;
+
+
+
+if(matrix[i][j] == 0) return 0;
+
+
+
+if(i > 0 && matrix[i-1][j] == 0) return 1;
+
+
+
+if(j > 0 && matrix[i][j-1] == 0) return 1;
+
+
+
+if(i < m - 1 && matrix[i+1][j] == 0) return 1;
+
+
+
+if(j < n-1 && matrix[i][j+1] == 0) return 1;
+
+
+
+// Don't have any neighboring zero'
+
+
+
+int top, left, right, bottom;
+
+
+
+top = left = 9999; // Declare them as infinity
+
+
+
+if(i > 0 && result[i-1][j] != 0){
+
+
+
+top = result[i-1][j];
+
+
+
+}
+
+
+
+if(j > 0 && result[i][j-1] != 0){
+
+
+
+left = result[i][j-1];
+
+
+
+}
+
+
+
+bottom = dfs(matrix, result, i+1, j);
+
+
+
+right = dfs(matrix, result, i, j+1 );
+
+
+
+return Math.min(Math.min(left, right), Math.min(top, bottom)) + 1;
+
+
+
+}
+
+
+
 }
