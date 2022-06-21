c7ac688467a512f192544d04e72092534ee39da6
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-20 20:09:50 -0700

    Time: 1 ms (100.00%), Space: 42.9 MB (86.72%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,34 +1,15 @@
 class Solution {
-    
-    HashSet<Integer> visited;
-    
     public boolean canJump(int[] nums) {
-        if(nums == null || nums.length<2) return true;
         
-        Queue<Integer> q = new LinkedList<>();
-        visited = new HashSet<>();
-        
-        q.add(0);
-        visited.add(0);
-        
-        //BFS
-        
-        while(!q.isEmpty())
+        int last = nums.length - 1;
+        for(int i = nums.length - 1;i>=0;i--)
         {
-            int curr = q.poll();
-            
-            for(int i = 1;i<=nums[curr];i++)
+            if(i + nums[i]>=last)
             {
-                int idx = i + curr;
-                
-                if(idx == nums.length - 1) return true;
-                if(!visited.contains(idx)) {
-                    q.add(idx);
-                    visited.add(idx);
-                }
+                last = i;
             }
         }
         
-        return false;
+        return last == 0;
     }
 }
