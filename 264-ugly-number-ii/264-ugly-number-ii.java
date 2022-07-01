7256027ee2ccf75bb2b0d13402952c5fc36ebdc3
Varun Gollapalli <gvarun1999@gmail.com>
2022-06-30 18:49:25 -0700

    Time: 4 ms (82.75%), Space: 42.1 MB (68.60%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,34 +1,34 @@
 class Solution {
     public int nthUglyNumber(int n) {
+        
         if(n == 1) return 1;
         
-        HashSet<Long> set = new HashSet<>();
-        PriorityQueue<Long> pq = new PriorityQueue<>();
+        int[] result = new int[n];
+        int p2=0,p3=0,p5=0;//indices
+        int n2=2,n3=3,n5=5;//vales
         
-        int count = 0;
-        long currNumber = 0;
-        int[] primes = {2,3,5};
-        
-        pq.add((long)1);
-        set.add((long)1);
-        
-        while(count<n)
+        result[0] = 1;
+        for(int i = 1;i<n;i++)
         {
-            currNumber = pq.poll();
-            count++;
-            
-            for(int prime: primes)
+            int min = Math.min(n2,Math.min(n3,n5));
+            result[i]=min;
+            if(min == n2)
             {
-                long newNumber = currNumber * prime;
-                
-                if(!set.contains(newNumber))
-                {
-                    set.add(newNumber);
-                    pq.add(newNumber);
-                }
-            }    
+                p2++;
+                n2 = result[p2]*2;
+            }
+            if(min == n3)
+            {
+                p3++;
+                n3 = result[p3]*3;
+            }
+            if(min == n5)
+            {
+                p5++;
+                n5 = result[p5]*5;
+            }
         }
         
-        return (int)currNumber;
+        return result[n-1];
     }
 }
