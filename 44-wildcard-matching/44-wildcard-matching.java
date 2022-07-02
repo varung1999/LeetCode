b855af8b216b02432e0614ff7f1a6858eede6c89
Varun Gollapalli <gvarun1999@gmail.com>
2022-07-01 18:44:01 -0700

    Time: 62 ms (23.51%), Space: 48.8 MB (41.43%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s.equals(p)) return true;
+        
+        int m = p.length();
+        int n = s.length();
+        
+        boolean[][] dp = new boolean[m+1][n+1];
+        
+        dp[0][0] = true;
+        
+        for(int i =1;i<=m;i++)
+        {
+            for(int j = 0;j<=n;j++)
+            {
+                if(p.charAt(i-1)!='*')
+                {
+                    if(j>0){
+                        
+                        if(p.charAt(i-1)==s.charAt(j-1) || p.charAt(i-1) =='?')
+                        {
+                            dp[i][j] = dp[i-1][j-1];
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    if(j>0){
+                        dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
