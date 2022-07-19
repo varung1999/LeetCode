21e814d2429d26ff3f7bffc2a71751bac96b4b45
Varun Gollapalli <gvarun1999@gmail.com>
2022-07-19 14:46:52 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (60.90%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        if(startValue == target) return 0;
+        
+        int count = 0;
+        
+        while(target> startValue)
+        {
+            if(target%2==0)
+            {
+                target/=2;
+            }
+            else
+            {
+                target++;
+            }
+            count++;
+        }
+        
+        return count + (startValue - target);
+    }
+}
