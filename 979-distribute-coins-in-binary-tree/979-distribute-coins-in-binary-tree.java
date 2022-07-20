85964bd45de89e2be90a4b2365c2fdfa0860b9aa
Varun Gollapalli <gvarun1999@gmail.com>
2022-07-20 11:08:40 -0700

    Time: 1 ms (62.61%), Space: 42.3 MB (68.52%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int moves;
+    public int distributeCoins(TreeNode root) {
+        if(root == null) return 0;
+        
+        
+        dfs(root);
+        return moves;
+    }
+    
+    private int dfs(TreeNode root){
+        if(root == null) return 0;
+        
+        int extra = root.val - 1;
+        extra+=dfs(root.left);
+        extra+=dfs(root.right);
+        moves+=Math.abs(extra);
+        return extra;
+    }
+}
