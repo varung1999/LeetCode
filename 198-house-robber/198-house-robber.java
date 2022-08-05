d6a66ad68be426d98a3ab0efd2a2d1bed7289c28
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-05 15:09:02 -0700

    Time: 0 ms (100.00%), Space: 39.8 MB (89.10%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        int skip=0,take=nums[0];
+        
+        for(int i =1;i<nums.length;i++)
+        {
+            int temp = skip;
+            skip = Math.max(skip,take);
+            take = temp + nums[i];
+        }
+        
+        return Math.max(skip,take);
+    }
+}
