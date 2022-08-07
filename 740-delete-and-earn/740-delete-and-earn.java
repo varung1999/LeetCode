16a08798143fac7a7e7dd5a07c6aa8a0a5d18fdb
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-07 10:57:16 -0700

    Time: 2 ms (99.19%), Space: 45.5 MB (81.99%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -3,29 +3,26 @@ class Solution {
         if(nums == null || nums.length == 0) return 0;
         
         int max = Integer.MIN_VALUE;
-        
         for(int i:nums)
         {
             max = Math.max(i,max);
         }
         
-        int[] dp = new int[max+1];
+        int[] temp = new int[max+1];
         
         for(int i:nums)
         {
-            dp[i]+=i;
+            temp[i]+=i;
         }
         
-        int skip = 0,take = dp[0];
-        
-        for(int i=1;i<dp.length;i++)
+        int skip = 0, take = temp[0];
+        for(int i=1;i<temp.length;i++)
         {
-            int temp = skip;
+            int x = skip;
             skip = Math.max(skip,take);
-            take = temp + dp[i];
+            take = x + temp[i]; 
         }
         
         return Math.max(skip,take);
-        
     }
 }
