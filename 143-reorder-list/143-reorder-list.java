2f3c7b361ee74d2eaf48f159873ab72d446d2171
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-16 16:16:04 -0700

    Time: 2 ms (77.80%), Space: 54.1 MB (54.70%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -14,7 +14,7 @@ class Solution {
         
         ListNode slow = head,fast = head;
         
-        while(fast.next!=null && fast.next.next!=null)
+        while(fast!=null && fast.next!=null)
         {
             slow = slow.next;
             fast = fast.next.next;
