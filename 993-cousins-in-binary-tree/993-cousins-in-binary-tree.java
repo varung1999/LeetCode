ccc9918277934e8215a6e3079ca91ba26adff1ea
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-17 17:57:14 -0700

    Time: 1 ms (65.88%), Space: 42.4 MB (22.77%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -19,33 +19,30 @@ class Solution {
         
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int level_x = -1;
-        int level_y = -2;
-        int depth = 0;
+       
         while(!q.isEmpty())
         {
             int size = q.size();
+            boolean x_found = false;
+            boolean y_found = false;
             for(int i =0;i<size;i++)
             {
                 TreeNode curr = q.poll();
+                if(curr.val == x) x_found = true;
+                if(curr.val == y) y_found = true;
                 
                 if(curr.left!=null && curr.right!=null)
                 {
                     if(curr.left.val == x && curr.right.val == y) return false;
                     if(curr.right.val == x && curr.left.val == y) return false;
                 }
-                
-                if(curr.val == x) level_x = depth;
-                if(curr.val == y)level_y = depth;
-                
                 if(curr.left!=null) q.add(curr.left);
                 if(curr.right!=null) q.add(curr.right);
             }
             
-            depth++;
+            if(x_found && y_found) return true;
         }
         
-        System.out.println(level_x +" "+level_y);
-        return level_x==level_y;
+        return false;
     }
 }
