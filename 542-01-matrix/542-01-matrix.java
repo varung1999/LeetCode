2b18eb23f5a77779f373c8e0f1f3e5bf0c845cdc
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-20 14:29:26 -0700

    Time: 21 ms (56.25%), Space: 72.5 MB (51.31%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,149 +1,48 @@
 class Solution {
-
-
-
-private int m;
-
-
-
-private int n;
-
-
-
-public int[][] updateMatrix(int[][] matrix) {
-
-
-
-m = matrix.length;
-
-
-
-// edge case
-
-
-
-if(matrix == null || m == 0) return matrix;
-
-
-
-n= matrix[0].length;
-
-
-
-int [][] result = new int [m][n];
-
-
-
-for(int i = 0; i < m; i++){
-
-
-
-for(int j = 0; j < n; j++){
-
-
-
-result[i][j] = dfs(matrix,result, i, j);
-
-
-
-}
-
-
-
-}
-
-
-
-return result;
-
-
-
-}
-
-
-
-private int dfs(int[][] matrix, int [][] result,int i, int j){
-
-
-
-// base case
-
-
-
-if(i < 0 || i >= m || j < 0 || j >= n) return 9999;
-
-
-
-if(matrix[i][j] == 0) return 0;
-
-
-
-if(i > 0 && matrix[i-1][j] == 0) return 1;
-
-
-
-if(j > 0 && matrix[i][j-1] == 0) return 1;
-
-
-
-if(i < m - 1 && matrix[i+1][j] == 0) return 1;
-
-
-
-if(j < n-1 && matrix[i][j+1] == 0) return 1;
-
-
-
-// Don't have any neighboring zero'
-
-
-
-int top, left, right, bottom;
-
-
-
-top = left = 9999; // Declare them as infinity
-
-
-
-if(i > 0 && result[i-1][j] != 0){
-
-
-
-top = result[i-1][j];
-
-
-
-}
-
-
-
-if(j > 0 && result[i][j-1] != 0){
-
-
-
-left = result[i][j-1];
-
-
-
-}
-
-
-
-bottom = dfs(matrix, result, i+1, j);
-
-
-
-right = dfs(matrix, result, i, j+1 );
-
-
-
-return Math.min(Math.min(left, right), Math.min(top, bottom)) + 1;
-
-
-
-}
-
-
-
+    public int[][] updateMatrix(int[][] mat) {
+        if(mat == null || mat.length == 0) return mat;
+        
+        Queue<int[]> q = new LinkedList<>();
+        
+        for(int i =0;i<mat.length;i++)
+        {
+            for(int j =0;j<mat[0].length;j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    q.add(new int[]{i,j});
+                }
+                else{
+                    mat[i][j] = -1;
+                }
+            }
+        }
+        
+        int level = 1;
+        
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i =0;i<size;i++){
+                int[] curr = q.poll();
+
+                for(int[] dir: dirs)
+                {
+                    int r = dir[0] + curr[0];
+                    int c = dir[1] + curr[1];
+
+                    if(r>=0 && r<mat.length && c>=0 && c<mat[0].length && mat[r][c] == -1)
+                    {
+                        q.add(new int[]{r,c});
+                        mat[r][c] = level;
+                    }
+                }
+            }
+            level++;
+        }
+        
+        return mat;
+    }
 }
