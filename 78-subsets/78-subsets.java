b8b49c3b968af90f240c6a7c5abaf9ed48a4a60d
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-22 14:28:41 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (93.51%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -4,22 +4,20 @@ class Solution {
         result = new ArrayList<>();
         if(nums == null || nums.length == 0) return result;
         
-        dfs(nums,0,new ArrayList<>());
+        result.add(new ArrayList<>());
+        
+        for(int i =0;i<nums.length;i++)
+        {
+            int size = result.size();
+            
+            for(int j =0;j<size;j++)
+            {
+                List<Integer> newList = new ArrayList<>(result.get(j));
+                newList.add(nums[i]);
+                result.add(newList);
+            }
+        }
         
         return result;
-    }
-    
-    private void dfs(int[] nums, int index, ArrayList<Integer> temp)
-    {
-        //base
-        
-        //logic
-        result.add(new ArrayList<>(temp));
-        for(int i =index;i<nums.length;i++)
-        {
-            temp.add(nums[i]);
-            dfs(nums,i+1,temp);
-            temp.remove(temp.size()-1);
-        }
     }
 }
