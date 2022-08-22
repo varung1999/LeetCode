e618b74f5eac58e2b202ba917393cfff3dd465b6
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-22 12:53:45 -0700

    Time: 6 ms (11.07%), Space: 41.2 MB (58.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n>1)
-        {
-            while(n%4==0)
-            {
-                n = n/4;
-            }
-        }
-        
-        return n==1;
+        return Integer.toString(n,4).matches("10*");
     }
 }
