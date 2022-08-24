11ff51577dbd4a6099f9ea62278b818c428da48d
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-23 19:24:07 -0700

    Time: 2 ms (72.24%), Space: 41.9 MB (11.00%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n>1)
+        {
+            while(n%2==0)
+            {
+                n=n/2;
+            }
+        }
+        
+        return n == 1;
+    }
+}
