c688cdbb2a50f81f6b67fa6877188ec1c710a808
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-26 16:10:57 -0700

    Time: 4 ms (67.36%), Space: 43.8 MB (48.57%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int thirdMax(int[] nums) {
-        Integer max1 = null;
-        Integer max2 = null;
-        Integer max3 = null;
-        for (Integer n : nums) {
-            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
-            if (max1 == null || n > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = n;
-            } else if (max2 == null || n > max2) {
-                max3 = max2;
-                max2 = n;
-            } else if (max3 == null || n > max3) {
-                max3 = n;
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i =0;i<nums.length;i++)
+        {
+            pq.add(nums[i]);
+            
+            if(pq.size()>k)
+            {
+                pq.poll();
             }
         }
-        return max3 == null ? max1 : max3;
+        
+        return pq.peek();
     }
 }
