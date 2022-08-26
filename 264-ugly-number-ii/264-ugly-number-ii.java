43349e06082af68be6db55a5856db6425e13db47
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-26 16:37:05 -0700

    Time: 385 ms (5.00%), Space: 53.8 MB (20.35%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,34 +1,20 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        
         if(n == 1) return 1;
         
-        int[] result = new int[n];
-        int p2=0,p3=0,p5=0;//indices
-        int n2=2,n3=3,n5=5;//vales
-        
-        result[0] = 1;
-        for(int i = 1;i<n;i++)
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        long ans = 1;
+        pq.add(ans);
+        for(int i =0;i<n;i++)
         {
-            int min = Math.min(n2,Math.min(n3,n5));
-            result[i]=min;
-            if(min == n2)
-            {
-                p2++;
-                n2 = result[p2]*2;
-            }
-            if(min == n3)
-            {
-                p3++;
-                n3 = result[p3]*3;
-            }
-            if(min == n5)
-            {
-                p5++;
-                n5 = result[p5]*5;
-            }
+            ans = pq.poll();
+            
+            if(!pq.contains(ans*2)) pq.add(ans*2);
+            if(!pq.contains(ans*3)) pq.add(ans*3);
+            if(!pq.contains(ans*5)) pq.add(ans*5);
         }
         
-        return result[n-1];
+        return (int)ans;
+        
     }
 }
