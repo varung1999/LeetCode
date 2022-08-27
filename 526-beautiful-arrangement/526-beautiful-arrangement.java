cd563121b540c9af3745b8eabca9195a783882fc
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-26 19:01:24 -0700

    Time: 77 ms (70.19%), Space: 40.9 MB (57.56%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -5,31 +5,31 @@ class Solution {
     public int countArrangement(int n) {
         if(n== 0 || n == 1) return n;
             
-        backtracking(n,1, new int[n+1]);
+        backtracking(n,1, new boolean[n+1]);
         
         return count;
     }
     
-    private void backtracking(int n, int index, int[] visited)
+    private void backtracking(int n, int index, boolean[] visited)
     {
         //base
         if(index>n)
         {
             count++;
-            return;
+            
         }
         
         //logic
         
         for(int i =1;i<=n;i++)
         {
-            if(visited[i]==0 && (i%index == 0 || index%i == 0))
+            if(visited[i]==false && (i%index == 0 || index%i == 0))
             {
-                visited[i] = 1;
+                visited[i] = true;
                 
                 backtracking(n,index+1,visited);
                 
-                visited[i] = 0;
+                visited[i] = false;
             }
         }
     }
