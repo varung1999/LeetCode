0f6fd6bfe88af4737232029f9d0c7be06bd5e5a4
Varun Gollapalli <gvarun1999@gmail.com>
2022-08-28 18:59:40 -0700

    Time: 18 ms (74.38%), Space: 47.7 MB (59.78%) - LeetHub

---
+++ 1286-iterator-for-combination/1286-iterator-for-combination.java
@@ -0,0 +1,40 @@
+class CombinationIterator {
+
+    PriorityQueue<String> pq;
+    public CombinationIterator(String characters, int combinationLength) {
+        pq = new PriorityQueue<>();
+        backtrack(characters,0,combinationLength, new StringBuilder());
+        
+    }
+    
+    private void backtrack(String s, int index, int count, StringBuilder result)
+    {
+        //base
+        if(result.length() == count)
+        {
+            pq.add(result.toString());
+        }
+        //logic
+        for(int i = index;i<s.length();i++)
+        {
+            result.append(s.charAt(i));
+            backtrack(s,i+1,count,result);
+            result.deleteCharAt(result.length()-1);
+        }
+    }
+    
+    public String next() {
+        return pq.poll();
+    }
+    
+    public boolean hasNext() {
+        return pq.size()>0;
+    }
+}
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator obj = new CombinationIterator(characters, combinationLength);
+ * String param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
