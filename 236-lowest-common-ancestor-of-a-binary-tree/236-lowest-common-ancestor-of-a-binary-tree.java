170f09a3094fd29deb4aa2b0f326f4dc59f6906a
Varun Gollapalli <gvarun1999@gmail.com>
2022-09-14 12:46:13 -0700

    Time: 14 ms (26.15%), Space: 48 MB (19.98%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,40 +8,15 @@
  * }
  */
 class Solution {
-    List<TreeNode> pathP;
-    List<TreeNode> pathQ;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        pathP = new ArrayList<>();
-        pathQ = new ArrayList<>();
+        if(root == null || root == p || root == q) return root;
         
-        backtracking(root,p,q, new ArrayList<>());
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
         
-        for(int i =0;i<pathP.size();i++)
-        {
-            if(pathP.get(i)!=pathQ.get(i)) return pathP.get(i-1);
-        }
-        
-        return null;
-    }
-    
-    private void backtracking(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> path)
-    {
-        //base
-        if(root == null) return;
-        
-        path.add(root);
-        if(root == p)
-        {
-            pathP = new ArrayList<>(path);
-            pathP.add(root);
-        }
-        if(root == q)
-        {
-            pathQ = new ArrayList<>(path);
-            pathQ.add(root);
-        }
-        backtracking(root.left,p,q,path);
-        backtracking(root.right,p,q,path);
-        path.remove(path.size()-1);
+        if(left == null && right == null) return null;
+        else if(left!=null && right == null) return left;
+        else if(left == null && right !=null) return right;
+        else return root;
     }
 }
