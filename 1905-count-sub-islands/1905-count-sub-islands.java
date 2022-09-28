47f02a8feff53eef336f6358ba612c2c9ff20674
Varun Gollapalli <gvarun1999@gmail.com>
2022-09-27 21:23:47 -0700

    Time: 41 ms (38.70%), Space: 126.5 MB (56.11%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,49 +1,22 @@
 class Solution {
-    int total;
-    boolean isIsland;
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        if(grid2 == null || grid2.length == 0) return 0;
-        
-        total = 0;
-        for(int i =0;i<grid2.length;i++)
-        {
-            for(int j = 0;j<grid2[0].length; j++)
-            {
-                if(grid2[i][j] == 1)
-                {
-                    isIsland = true;
-                    dfs(grid1,grid2,i,j);
-                        
-                        if(isIsland)
-                        {
-                            total++;
-                        } 
-                }
-            }
-        }
-        
-        return total;
-        
+        int result = 0;
+        for(int i=0;i<grid2.length;i++)
+            for(int j=0;j<grid2[0].length;j++)
+                if(grid2[i][j] == 1 && isSubIsland(grid1, grid2, i , j)) result++;
+        return result;
     }
-    
-    private void dfs(int[][] grid1,int[][] grid2,int r,int c)
-    {
-        //base
-        if(r<0 || c<0 || r>=grid1.length || c>=grid1[0].length || grid2[r][c]!=1) return;
-        
-        if(grid1[r][c]!=grid2[r][c]){
-            isIsland = false;
+    private boolean isSubIsland(int[][] grid1, int[][] grid2, int i, int j) {
+        boolean result = true;
+        if(i>=0 && i<grid2.length && j>=0 && j<grid2[0].length && grid2[i][j] == 1) {
+            if(grid2[i][j] != grid1[i][j]) result =false;
+            grid2[i][j] = -1; // Mark Visited
+            boolean left = isSubIsland(grid1, grid2, i, j -1);
+            boolean right = isSubIsland(grid1, grid2, i, j +1);
+            boolean top = isSubIsland(grid1, grid2, i-1, j);
+            boolean bottom = isSubIsland(grid1, grid2, i+1, j);
+            if(!left || !right || !top || !bottom) return false;
         }
-        
-        //logic
-        grid1[r][c]=0;
-        grid2[r][c] = 0;
-        
-        dfs(grid1,grid2,r-1,c);
-        dfs(grid1,grid2,r+1,c);
-        dfs(grid1,grid2,r,c-1);
-        dfs(grid1,grid2,r,c+1);
-        
-        
+        return result;
     }
 }
