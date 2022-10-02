36f9420c25140d7089e5ccdd49b07101124e6965
Varun Gollapalli <gvarun1999@gmail.com>
2022-10-01 23:31:09 -0700

    Time: 29 ms (58.51%), Space: 52.4 MB (21.97%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,20 +1,30 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int index = nums.length - 1;
+        if(nums == null || nums.length == 0) return 0;
         
-        return dp(nums,target,index,0);
-    }
-    
-    private int dp(int[] nums, int target, int index, int currSum)
-    {
-        //base
-        if(index<0 && currSum == target) return 1;
-        if(index<0) return 0;
+        int sum = 0;
+        for(int i:nums)sum+=i;
+        if(target<-sum || target>sum) return 0;
         
-        //logic
-        int pos = dp(nums,target,index-1,currSum+nums[index]);
-        int neg = dp(nums,target,index-1,currSum - nums[index]);
+        int[][] dp = new int[nums.length+1][2*sum+1];
+        dp[0][sum]=1;
         
-        return pos + neg;
+        for(int i = 1;i<dp.length;i++)
+        {
+            for(int j = 0;j<dp[0].length;j++)
+            {
+                if(j + nums[i-1]<=sum*2)
+                {
+                    dp[i][j]+=dp[i-1][j+nums[i-1]];
+                }
+                if(j - nums[i-1]>=0)
+                {
+                    dp[i][j]+=dp[i-1][j-nums[i-1]];
+                }
+            }
+        }
+        
+        return dp[nums.length][sum+target];
+        
     }
 }
