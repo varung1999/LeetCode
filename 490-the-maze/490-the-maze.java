bb0e87de5e4499126480aaf2219f9340b67c7455
Varun Gollapalli <gvarun1999@gmail.com>
2022-10-11 16:04:04 -0700

    Time: 12 ms (32.58%), Space: 54.8 MB (19.75%) - LeetHub

---
+++ 490-the-maze/490-the-maze.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        if(maze == null || maze.length == 0) return false;
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start[0],start[1]});
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        maze[start[0]][start[1]] = 2;
+        while(!q.isEmpty())
+        {
+            int[] curr = q.poll();
+            
+            for(int[] dir: dirs)
+            {
+                int r = curr[0];
+                int c = curr[1];
+                
+                while(r>=0 && r< maze.length && c>=0 && c < maze[0].length && maze[r][c]!=1)
+                {
+                    r = r + dir[0];
+                    c = c + dir[1];
+                    
+                }
+                
+                r = r - dir[0];
+                c = c - dir[1];
+                
+                if(maze[r][c]!=2)
+                {
+                    if(r == destination[0] && c == destination[1])
+                    {
+                        return true;
+                    }
+                    
+                    maze[r][c] = 2;
+                    q.add(new int[]{r,c});
+                }
+            }
+        }
+        
+        return false;
+        
+    }
+}
