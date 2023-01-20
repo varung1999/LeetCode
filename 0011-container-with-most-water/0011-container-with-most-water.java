3b46f06341fd35aab2f47f1a0bc24cf1a402302b
Varun Gollapalli <gvarun1999@gmail.com>
2023-01-20 06:11:38 -0700

    Time: 3 ms (94.61%), Space: 52.6 MB (70.05%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length == 0) return 0;
+        int result = Integer.MIN_VALUE;
+        int left = 0, right = height.length - 1;
+        
+        while(left<right)
+        {
+            int min = Math.min(height[left],height[right]);
+            result = Math.max(result, min * (right - left));
+            if(min == height[left])
+            {
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return result;
+    }
+}
