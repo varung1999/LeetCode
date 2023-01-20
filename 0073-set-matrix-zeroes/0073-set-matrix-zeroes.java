df974b44f789f06b11a5d2eba3646e74c6c4092d
Varun Gollapalli <gvarun1999@gmail.com>
2023-01-19 23:53:19 -0700

    Time: 0 ms (100.00%), Space: 43.8 MB (76.55%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col0 = 1, m = matrix.length, n = matrix[0].length;
+        
+        for(int i = 0;i<m;i++)
+        {
+            if(matrix[i][0]==0) col0=0;
+            for(int j = 1;j<n;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        // for(int i=0;i<matrix.length;i++)
+        // {
+        //     for(int j = 0;j<matrix[0].length;j++)
+        //     {
+        //         System.out.print(matrix[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        for(int i = m - 1;i>=0;i--)
+        {
+            for(int j = n-1;j>=1;j--)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+            if(col0==0)
+            {
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
