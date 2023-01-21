a0bbae904731a679432d4752e88ac424dd6181fb
Varun Gollapalli <gvarun1999@gmail.com>
2023-01-21 05:24:21 -0700

    Time: 31 ms (20.63%), Space: 49.3 MB (66.85%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -4,30 +4,28 @@ class Solution {
         List<Integer> order = new ArrayList<Integer>();
         backtrack(nums,0,order,result);
         return new ArrayList(result);
-        
     }
-    
     private void backtrack(int[] nums, int index, List<Integer> order, Set<List<Integer>> result)
     {
         //base
         if(index==nums.length)
         {
+            for(int i = 1; i < order.size(); i++) {
+                if(order.get(i) < order.get(i - 1)) {
+                    return;
+                }
+            }
             if(order.size()>=2)
             {
                 result.add(new ArrayList<>(order));
             }
             return;
         }
-        
         //logic
-        if(order.isEmpty() || order.get(order.size()-1)<=nums[index])
-        {
-            order.add(nums[index]);   
-            backtrack(nums,index+1,order,result);
-            order.remove(order.size()-1);
-            
-        }
+        order.add(nums[index]);
+        //System.out.println(order+" at i = "+index);
         backtrack(nums,index+1,order,result);
-        
+        order.remove(order.size()-1);
+        backtrack(nums,index+1,order,result);
     }
 }
