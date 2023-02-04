5e18efaabc525d8eb8182c1540c5291ba0675fb0
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-03 17:50:14 -0700

    Time: 7 ms (65.80%), Space: 41.8 MB (96.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()> s2.length()) return false;
+        if(s1.length()>s2.length()) return false;
         
         int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        
         for(int i = 0;i<s1.length();i++)
         {
+            s2map[s2.charAt(i)-'a']++;
             s1map[s1.charAt(i)-'a']++;
         }
         
-        for(int i = 0;i<=s2.length()-s1.length();i++)
+        for(int i = 0;i<s2.length() - s1.length();i++)
         {
-            int[] s2map = new int[26];
-            for(int j =0;j<s1.length();j++)
-            {
-                s2map[s2.charAt(i+j)-'a']++;
-            }
-            
-            if(matches(s2map,s1map)) return true;
+            if(matches(s1map,s2map)) return true;
+            s2map[s2.charAt(i+s1.length())-'a']++;
+            s2map[s2.charAt(i)-'a']--;
         }
         
-        return false;
+        return matches(s1map,s2map);
     }
     
     private boolean matches(int[] a, int[] b)
     {
-        for(int i = 0;i<26;i++)
+        for(int i=0;i<26;i++)
         {
             if(a[i]!=b[i]) return false;
         }
