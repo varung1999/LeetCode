6fea68eb525b2eb84a062109bf6ec11af7530b50
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-08 12:18:13 -0800

    Time: 3 ms (40.07%), Space: 43.4 MB (26.78%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.java
+++ 0280-wiggle-sort/0280-wiggle-sort.java
@@ -2,11 +2,14 @@ class Solution {
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
         
-        for(int i = 1;i<nums.length - 1;i+=2)
+        for(int i = 0;i<nums.length - 1;i++)
         {
-            int temp = nums[i];
-            nums[i] = nums[i+1];
-            nums[i+1] = temp;
+            if(((i%2==0) && nums[i]>nums[i+1]) || (i%2==1) && nums[i]<nums[i+1])
+            {
+                int temp = nums[i];
+                nums[i]=nums[i+1];
+                nums[i+1] = temp;
+            }
         }
     }
 }
