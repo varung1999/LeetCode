1904d235ea566af326af38af7f0b52da757cb324
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-17 15:02:17 -0800

    Time: 3 ms (38.36%), Space: 44.9 MB (76.24%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int minPathSum(int[][] grid) {
         if(grid == null || grid.length == 0) return 0;
         
-        int[][] dp = new int[grid.length][grid[0].length];
+        int[] dp = new int[grid[0].length];
         
         for(int i = grid.length-1;i>=0;i--)
         {
@@ -11,27 +11,27 @@ class Solution {
                 //last row but not last col
                 if(i==grid.length-1 && j!=grid[0].length-1)
                 {
-                    dp[i][j] = dp[i][j+1] + grid[i][j];
+                    dp[j] = grid[i][j] + dp[j+1];
                 }
                 
                 //last col but not last row
                else if(j==grid[0].length-1 && i!=grid.length-1)
                 {
-                    dp[i][j] = grid[i][j] + dp[i+1][j];
+                    dp[j] = grid[i][j] + dp[j];
                 }
                 
                 //not last row && not last col
                 
                 else if(i!=grid.length-1 && j!=grid[0].length-1)
                 {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i+1][j],dp[i][j+1]);
+                    dp[j] = grid[i][j] + Math.min(dp[j],dp[j+1]);
                 }
                 
                 else{
-                    dp[i][j] = grid[i][j];
+                    dp[j] = grid[i][j];
                 }
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
