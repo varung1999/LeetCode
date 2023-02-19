bb64cecffdf3fc91c3ff534088522c04bec1ac69
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-19 08:59:06 -0800

    Time: 16 ms (18.67%), Space: 43 MB (30.77%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        HashSet visited = new HashSet();
+        
+        for(int i = 0;i<board.length;i++)
+        {
+            for(int j = 0;j<board[0].length;j++)
+            {
+                char curr = board[i][j];
+                
+                if(curr!='.')
+                {
+                    if(!visited.add(curr + " in row " + i) || !visited.add(curr +" in column "+j) || !visited.add(curr + " in block "+ i/3 +" - " + j/3))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
