1e8bbcdcf5ce92d00536e8d9893db20f34e240e5
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-19 16:11:29 -0800

    Time: 1 ms (99.83%), Space: 42.5 MB (71.52%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     List<List<Integer>> result;
-    HashSet<List<Integer>> visited;
+    //HashSet<List<Integer>> visited;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         result = new ArrayList<>();
-        visited = new HashSet<>();
-        Arrays.sort(nums);
         if(nums == null || nums.length == 0) return result;
-        
+        Arrays.sort(nums);
         dfs(nums,0,new ArrayList<>());
         
         return result;
@@ -15,21 +13,17 @@ class Solution {
     private void dfs(int[] nums, int index, ArrayList<Integer> path)
     {
         //base
-        if(index == nums.length)
-        {
-            if(!visited.contains(path))
-            {
-                result.add(new ArrayList<>(path));
-            }
-            
-            visited.add(new ArrayList<>(path));
-            return;
-        }
+        
         
         //logic
-        dfs(nums,index+1,path);
-        path.add(nums[index]);
-        dfs(nums,index+1,path);
-        path.remove(path.size()-1);
+        result.add(new ArrayList<>(path));
+        for(int i = index;i<nums.length;i++)
+        {
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            
+            path.add(nums[i]);
+            dfs(nums,i+1,path);
+            path.remove(path.size()-1);
+        }
     }
 }
