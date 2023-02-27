21f7c0f96caa276f54b9a67933ebfbde8af16f83
Varun Gollapalli <gvarun1999@gmail.com>
2023-02-26 17:51:35 -0800

    Time: 6 ms (99.87%), Space: 42.7 MB (55.39%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    class TrieNode{
+        TrieNode[] children;
+        String word;
+        
+        public TrieNode(){
+            children = new TrieNode[26];
+        }
+    }
+    
+    TrieNode root;
+    
+    private void insert(String word)
+    {
+        TrieNode curr = root;
+        for(int i = 0;i<word.length();i++)
+        {
+            char c = word.charAt(i);
+            
+            if(curr.children[c- 'a'] == null){
+                curr.children[c- 'a'] = new TrieNode();
+            } 
+            
+            curr = curr.children[c-'a'];
+        }
+        
+        curr.word = word;
+    }
+    
+    public String longestWord(String[] words) {
+        if(words == null || words.length == 0) return "";
+        root = new TrieNode();
+        for(String word: words)
+        {
+            insert(word);
+        }
+        
+        Queue<TrieNode> q = new LinkedList<>();
+        q.add(root);
+        TrieNode curr = new TrieNode();
+        while(!q.isEmpty())
+        {
+            curr = q.poll();
+            
+            for(int i = 25;i>=0;i--)
+            {
+                if(curr.children[i]!=null && curr.children[i].word!=null)
+                {
+                    q.add(curr.children[i]);
+                }
+            }
+        }
+        
+        if(curr.word==null) return "";
+        return curr.word;
+    }
+}
