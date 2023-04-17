6c07bb2823e29c9fb720a01c8a1d43ef8e0f4781
Varun Gollapalli <gvarun1999@gmail.com>
2023-04-17 04:58:08 -0700

    Time: 1 ms (99.68%), Space: 41.8 MB (84.69%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[] parent = new int[26];
+    public boolean equationsPossible(String[] equations) {
+        
+        for(int i = 0;i<26;i++)
+        {
+            parent[i]=i;
+        }
+        
+        for(String e: equations)
+        {
+            if(e.charAt(1)=='=') parent[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
+        }
+        
+        for(String e: equations)
+        {
+            if(e.charAt(1)=='!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a')) return false;
+        }
+        
+        return true;
+    }
+    
+    private int find(int x)
+    {
+        if(parent[x] == x) return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+}
