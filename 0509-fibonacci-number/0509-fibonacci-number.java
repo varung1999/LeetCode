18d4c28fb2c61a83d47e47a5708df0f8db30863f
Varun Gollapalli <gvarun1999@gmail.com>
2023-04-23 11:34:12 -0700

    Time: 0 ms (100.00%), Space: 40.1 MB (8.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
     public int fib(int n) {
-        return helper(n);
-    }
-    
-    private int helper(int n)
-    {
-        //base
-        if(n==0 || n == 1) return n;
+        if(n == 0 || n == 1) return n;
         
-        //logic
-        int one = helper(n-1);
-        int two = helper(n-2);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         
-        return one + two;
+        return dp[n];
     }
 }
