600bca1e30573cce7762f5b8aed0b5b3685cbd30
Varun Gollapalli <gvarun1999@gmail.com>
2023-04-26 12:34:10 -0700

    Time: 2 ms (43.26%), Space: 43.8 MB (18.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int ans = 0, n = nums.length;
-        int currEnd = 0, currFar = 0;
+        int count = 0;
         
-        for(int i = 0;i<n-1;i++)
+        int currFar = 0, currEnd = 0;
+        for(int i = 0;i<nums.length - 1;i++)
         {
             currFar = Math.max(currFar,i+nums[i]);
             
-            if(i==currEnd)
+            if(i == currEnd)
             {
-                ans++;
+                count++;
                 currEnd = currFar;
             }
         }
-        return ans;
+        
+        return count;
     }
 }
