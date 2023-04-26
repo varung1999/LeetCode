a7c1a1bbfb1cf542e90a80f027134e6574bdaece
Varun Gollapalli <gvarun1999@gmail.com>
2023-04-26 12:28:25 -0700

    Time: 1 ms (99.98%), Space: 43.3 MB (39.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int last = nums.length - 1;
-       for(int i = nums.length - 1;i>=0;i--)
-       {
-           if(i+nums[i]>=last)
-           {
-               last = i;
-           }
-       }
-        return last == 0;
+        if(nums == null || nums.length == 0) return false;
+        
+        int lastPos = nums.length - 1;
+        
+        for(int i = nums.length - 1;i>=0;i--)
+        {
+            if(i+nums[i]>=lastPos)
+            {
+                lastPos = i;
+            }
+        }
+        
+        return lastPos == 0;
     }
 }
