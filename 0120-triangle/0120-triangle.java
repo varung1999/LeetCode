54c51b2b20ec276af2d5f0bb83270076b9778240
Varun Gollapalli <gvarun1999@gmail.com>
2023-05-17 20:31:26 -0700

    Time: 8 ms (21.21%), Space: 42.5 MB (28.62%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,12 +3,13 @@ class Solution {
         if(triangle == null || triangle.size() == 0) return 0;
         
         int n = triangle.size();
-        for(int i = n-2;i>=0;i--)
+        
+        for(int i = n - 2;i>=0;i--)
         {
             for(int j = 0;j<=i;j++)
             {
                 int min = Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
-                triangle.get(i).set(j,min+triangle.get(i).get(j));
+                triangle.get(i).set(j,min + triangle.get(i).get(j));
             }
         }
         
