7816abc27b346f22a1b41fe0791c52f35337bd54
Varun Gollapalli <gvarun1999@gmail.com>
2023-06-05 15:53:11 -0700

    Time: 2 ms (61.07%), Space: 40.6 MB (60.21%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        
+        Arrays.sort(citations);
+        int n = citations.length;
+        int low = 0, high = citations.length - 1;
+        
+        while(low<=high)
+        {
+            int mid = low + (high - low)/2;
+            if(citations[mid] == n - mid) return n - mid;
+            else if(citations[mid]<n - mid) low = mid + 1;
+            else high = mid - 1;
+        }
+        
+        return n - low;
+        
+    }
+}
