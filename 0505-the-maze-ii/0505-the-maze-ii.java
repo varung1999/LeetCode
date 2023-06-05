aabaf31d433b11b2bd4a8e2c17a83e24220d908d
Varun Gollapalli <gvarun1999@gmail.com>
2023-06-05 15:18:56 -0700

    Time: 8 ms (70.31%), Space: 45.3 MB (8.12%) - LeetHub

---
+++ 0505-the-maze-ii/0505-the-maze-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        
+        int[][] distance = new int[maze.length][maze[0].length];
+        for(int[] dist: distance) Arrays.fill(dist,Integer.MAX_VALUE);
+        
+        distance[start[0]][start[1]] = 0;
+        
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start[0],start[1]});
+                
+        while(!q.isEmpty())
+        {
+            int[] curr = q.poll();
+            int distanceTillCurr = distance[curr[0]][curr[1]];
+            for(int[] dir: dirs)
+            {
+                int r = curr[0] + dir[0];
+                int c = curr[1] + dir[1];
+                int pathCount = distanceTillCurr + 1;
+                while(r>=0 && c>=0 && r<maze.length && c<maze[0].length && maze[r][c] == 0)
+                {
+                    r = r + dir[0];
+                    c = c + dir[1];
+                    pathCount++; 
+                }
+                
+                r = r - dir[0];
+                c = c - dir[1];
+                
+                pathCount--;
+                
+                if(!Arrays.equals(curr,destination) && pathCount<distance[r][c])
+                {
+                    distance[r][c] = pathCount;
+                    q.add(new int[]{r,c});
+                }
+            }
+        }
+        
+        int res = distance[destination[0]][destination[1]];
+        
+        return res == Integer.MAX_VALUE?-1:res;
+    }
+}
