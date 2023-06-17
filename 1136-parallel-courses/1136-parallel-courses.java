8fb21da62e892b09d5b2461ff22452bc5eb8dd0c
Varun Gollapalli <gvarun1999@gmail.com>
2023-06-17 15:44:03 -0700

    Time: 10 ms (70.08%), Space: 45.5 MB (43.12%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        int[] inCount = new int[n+1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0;i<n+1;i++)
+        {
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] relation: relations)
+        {
+            graph.get(relation[0]).add(relation[1]);
+            inCount[relation[1]]++;
+        }
+        
+        int step = 0;
+        int studiedCount = 0;
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1;i<n+1;i++)
+        {
+            if(inCount[i] == 0) q.add(i);
+        }
+        
+        while(!q.isEmpty())
+        {
+            step++;
+            Queue<Integer> nextQueue = new LinkedList<>();
+            
+            for(int node:q)
+            {
+                studiedCount++;
+                
+                for(int endNode: graph.get(node))
+                {
+                    inCount[endNode]--;
+                    
+                    if(inCount[endNode]==0) nextQueue.add(endNode);
+                }
+            }
+            
+            q = nextQueue;
+        }
+        
+        return studiedCount == n ? step: -1;
+    }
+}
