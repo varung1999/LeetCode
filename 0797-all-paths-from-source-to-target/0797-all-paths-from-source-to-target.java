5975da8aeb54af0def6fd06723f6b04c469c4475
Varun Gollapalli <gvarun1999@gmail.com>
2023-06-18 14:27:26 -0700

    Time: 2 ms (93.34%), Space: 45.8 MB (19.50%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,40 +1,51 @@
 class Solution {
     List<List<Integer>> result;
-    
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         result = new ArrayList<>();
-        if (graph == null || graph.length == 0) return result;
+        if(graph == null || graph.length == 0) return result;
         
         int n = graph.length;
         List<List<Integer>> map = new ArrayList<>();
         
-        for (int i = 0; i < n; i++) {
+        for(int i =0;i<n;i++)
+        {
             List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < graph[i].length; j++) {
+            
+            for(int j=0;j<graph[i].length;j++)
+            {
                 row.add(graph[i][j]);
             }
             map.add(row);
         }
         
-        dfs(map, 0, new ArrayList<>(), new boolean[n], n);
+        dfs(map,0,new ArrayList<>(), new boolean[n]);
         
         return result;
     }
     
-    private void dfs(List<List<Integer>> map, int curr, List<Integer> path, boolean[] visited, int size) {
-        path.add(curr); // Add the current node to the path
+    private void dfs(List<List<Integer>> map, int curr, ArrayList<Integer> path, boolean[] visited)
+    {
+        //base
+        path.add(curr);
+        if(curr == visited.length - 1)
+        {
+            
+            result.add(new ArrayList<>(path));
+            return;
+        }
         
-        if (curr == size - 1) {
-            result.add(new ArrayList<>(path)); // Reached the target node, add the current path to the result
-        } else {
-            for (int child : map.get(curr)) {
-                if (!visited[child]) {
-                    visited[child] = true; // Mark the child as visited
-                    dfs(map, child, path, visited, size); // Recursive DFS traversal on the child node
-                    visited[child] = false; // Backtrack: Mark the child as unvisited for other paths
-                    path.remove(path.size() - 1); // Remove the current node from the path to backtrack
-                }
+        //logic
+        for(int child: map.get(curr))
+        {
+            if(!visited[child])
+            {
+                visited[child] = true;
+                dfs(map,child,path,visited);
+                visited[child] = false;
+                path.remove(path.size()-1);
             }
+            
+            
         }
     }
 }
