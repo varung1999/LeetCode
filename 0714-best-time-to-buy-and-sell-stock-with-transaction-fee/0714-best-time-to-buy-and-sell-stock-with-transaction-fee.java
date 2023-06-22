2bd3b66e4004e70a006c98dc0c8b6e7e7c314eb4
Varun Gollapalli <gvarun1999@gmail.com>
2023-06-21 17:42:58 -0700

    Time: 4 ms (99.03%), Space: 54.5 MB (57.79%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -3,16 +3,15 @@ class Solution {
         if(prices == null || prices.length == 0) return 0;
         
         int n = prices.length;
-        int[] free = new int[n], hold = new int[n];
-        
-        hold[0] = - prices[0];
+        int free = 0, hold = - prices[0];
         
         for(int i = 1;i<n;i++)
         {
-            hold[i] = Math.max(hold[i-1],free[i-1] - prices[i]);
-            free[i] = Math.max(free[i-1], hold[i-1] + prices[i] - fee);
+            int temp = hold;
+            hold = Math.max(hold, free - prices[i]);
+            free = Math.max(free, temp + prices[i] - fee);
         }
         
-        return free[n-1];
+        return free;
     }
 }
