88b013315fb544a351aec659162bcad22ecdb391
Varun Gollapalli <gvarun1999@gmail.com>
2023-07-30 13:46:01 -0700

    Time: 0 ms (100.00%), Space: 40.2 MB (78.40%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -3,14 +3,19 @@ class Solution {
         if(nums == null || nums.length<3) return 0;
         
         int count = 0,n = nums.length;
-        int[] dp = new int[n];
+        int currCount = 0;
         
         for(int i = 2;i<n;i++)
         {
-            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1]) dp[i] = 1 + dp[i-1];
+            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1]){
+                currCount+=1;
+                count+=currCount;
+            }
+            else{
+                currCount = 0;
+            }
         }
-        
-        for(int i:dp) count+=i;
+         
         return count;
     }
 }
