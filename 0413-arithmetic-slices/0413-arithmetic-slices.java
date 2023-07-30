48261766e4946f0268a5fb396c1e6c385ea37df0
Varun Gollapalli <gvarun1999@gmail.com>
2023-07-30 13:35:16 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (55.57%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -3,18 +3,14 @@ class Solution {
         if(nums == null || nums.length<3) return 0;
         
         int count = 0,n = nums.length;
+        int[] dp = new int[n];
         
-        for(int i = 0;i<n-1;i++)
+        for(int i = 2;i<n;i++)
         {
-            int diff = nums[i+1] - nums[i];
-            
-            for(int j = i+1;j<n-1;j++)
-            {
-                if(nums[j+1]-nums[j] == diff) count++;
-                else break;
-            }
+            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1]) dp[i] = 1 + dp[i-1];
         }
         
+        for(int i:dp) count+=i;
         return count;
     }
 }
