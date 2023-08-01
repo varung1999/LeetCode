80fbf462ff87f12167461b71e564d6fdf549b750
Varun Gollapalli <gvarun1999@gmail.com>
2023-08-01 12:26:34 -0700

    Time: 9 ms (84.05%), Space: 45.5 MB (51.22%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> result;
+    HashSet<List<Integer>> set;
+    public List<List<Integer>> combine(int n, int k) {
+        result = new ArrayList<>();
+        set = new HashSet<>();
+        if(n == 0) return result;
+        
+        dfs(n,k,1,new ArrayList<>());
+        
+        return result;
+    }
+    
+    private void dfs(int n, int k,int curr, ArrayList<Integer> path)
+    {
+        //base
+        if(path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        
+        //if(curr >= n) return;
+        
+        //logic
+       for(int num = curr;num<=n;num++)
+       {
+           path.add(num);
+           dfs(n,k,num+1,path);
+           path.remove(path.size()-1);
+       }
+    }
+}
