2f4f73ed315fb6251124c084d3a50db7959adab6
Varun Gollapalli <gvarun1999@gmail.com>
2023-08-22 09:53:42 -0700

    Time: 1 ms (97.61%), Space: 45.9 MB (72.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,33 +1,67 @@
+class UnionFind{
+    
+    int[] parent;
+    int n;
+    
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        this.n = n;
+        
+        for(int i =0;i<n;i++) parent[i] = i;
+    }
+    
+    public void union(int u, int v)
+    {
+        if(u!=v)
+        {
+            int a = find(u);
+            int b = find(v);
+            
+            parent[a] = b;
+        }
+    }
+    
+    public int find(int u)
+    {
+        int x = u;
+        while(x!=parent[x]) x = parent[x];
+        
+        parent[u] = x;
+        
+        return x;
+    }
+    
+    public boolean areConnected(int u, int v)
+    {
+        return find(u) == find(v);
+    }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         if(isConnected == null || isConnected.length == 0) return 0;
         
-        int n = isConnected.length;
-        int count = 0;
+        UnionFind uf = new UnionFind(isConnected.length);
         
-        boolean visited[] = new boolean[n];
-        
-        for(int i = 0;i<n;i++)
+        for(int i = 0;i<isConnected.length;i++)
         {
-            if(!visited[i])
+            for(int j = i+1; j<isConnected[i].length;j++)
             {
-                count++;
-                dfs(i,isConnected,visited);
+                
+                if(isConnected[i][j] == 1)
+                {
+                    uf.union(i,j);
+                }
             }
+        }
+        
+       int count = 0;
+        for(int i = 0;i<uf.parent.length;i++)
+        {
+            if(uf.parent[i] == i) count++;
         }
         
         return count;
-    }
-    
-    private void dfs(int curr,int[][] isConnected,boolean[] visited)
-    {
-        visited[curr] = true;
-        for(int i = 0;i<isConnected.length;i++)
-        {
-            if(isConnected[curr][i] == 1 && !visited[i])
-            {
-                dfs(i,isConnected,visited);
-            }
-        }
     }
 }
