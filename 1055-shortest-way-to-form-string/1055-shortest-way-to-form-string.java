3db8679fd6854769f2fe1cb7ffbaa6b2af04a1d9
Varun Gollapalli <gvarun1999@gmail.com>
2023-09-01 14:45:17 -0700

    Time: 6 ms (51.24%), Space: 40.4 MB (76.73%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,31 +1,40 @@
 class Solution {
     public int shortestWay(String source, String target) {
         
-        int[] count = new int[26];
-        for(char c:source.toCharArray()) {
-            count[c-'a']++;
-        }
+//         int[] count = new int[26];
+//         for(char c:source.toCharArray()) {
+//             count[c-'a']++;
+//         }
         
-        for(char c:target.toCharArray()){
-            if(count[c-'a']==0) return -1;
-        }
+//         for(char c:target.toCharArray()){
+//             if(count[c-'a']==0) return -1;
+//         }
         
-        int result = 1, sourceIndex = 0, targetIndex = 0;
+        int result = 0, targetIndex = 0;
         
-        while(targetIndex<target.length())
-        {
-            if(sourceIndex >= source.length())
-            {
-                result++;
-                sourceIndex = 0; 
-            }
-            if(source.charAt(sourceIndex)==target.charAt(targetIndex))
-            {
+        while (targetIndex < target.length()) {
+        int sourceIndex = 0;
+        int prevTargetIndex = targetIndex;
+
+        while (sourceIndex < source.length() && targetIndex < target.length()) {
+            if (source.charAt(sourceIndex) == target.charAt(targetIndex)) {
+                sourceIndex++;
                 targetIndex++;
+            } else {
+                sourceIndex++;
             }
-            sourceIndex++;
         }
-        
-        return result;
+
+        if (prevTargetIndex == targetIndex) {
+            // If targetIndex didn't change, it means no progress was made,
+            // and we can't match the characters anymore.
+            return -1;
+        }
+
+        result++;
     }
+
+    return result;
+    }
+
 }
