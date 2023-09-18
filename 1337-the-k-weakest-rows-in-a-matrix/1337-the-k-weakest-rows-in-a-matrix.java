d427d9e8cb41ecb555a5a59bde48030d1d50190b
Varun Gollapalli <gvarun1999@gmail.com>
2023-09-18 12:36:06 -0700

    Time: 1 ms (99.31%), Space: 44.9 MB (17.49%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -2,30 +2,22 @@ class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
         if(mat == null || mat.length == 0) return new int[]{};
         
-        ArrayList<int[]> list = new ArrayList<>();
-        Comparator<int[]> comparator = new Comparator<int[]>(){
-            public int compare(int[] a, int[] b)
-            {
-                return a[1] == b[1] ? a[0] - b[0] : a[1] - b[1];
-            }
-        };
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+           if(a[1] == b[1]) return b[0] - a[0];
+            else return b[1] - a[1];
+        });
         for(int i = 0;i<mat.length;i++)
         {
             
             int count = binarySearch(mat[i]);
-            list.add(new int[]{i,count});
-        }
-        Collections.sort(list,comparator);
-        for(int[] temp: list)
-        {
-            System.out.println(temp[0] +" "+temp[1]);
+            pq.add(new int[]{i,count});
+            if(pq.size()>k) pq.poll();
         }
         
         int[] res = new int[k];
-        for(int i = 0;i<k;i++)
+        for(int i = k-1;i>=0;i--)
         {
-            int[] curr = list.get(i);
+            int[] curr = pq.poll();
             res[i] = curr[0];
         }
         
