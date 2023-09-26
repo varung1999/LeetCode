c4afdf340e2c1ebe13b0b675a05964385ae898af
Varun Gollapalli <gvarun1999@gmail.com>
2023-09-26 12:40:30 -0700

    Time: 1 ms (93.29%), Space: 40.2 MB (90.43%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -3,7 +3,7 @@ class Solution {
         if(s == null || s.length() == 0) return 0;
         
         int count = 0;
-        for(int i = 0;i<s.length()-3;i++)
+        for(int i = 0;i<s.length()-2;i++)
         {
             char first = s.charAt(i);
             char second = s.charAt(i+1);
@@ -12,9 +12,6 @@ class Solution {
                 count++;
             }
         }
-        int n = s.length();
-        boolean last = false;
-        if(n>=3) last = (s.charAt(n-3) !=s.charAt(n-2) && s.charAt(n-2)!=s.charAt(n-1) && s.charAt(n-3)!=s.charAt(n-1));
-        return last == true? count +1: count;
+       return count;
     }
 }
