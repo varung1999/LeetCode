a083a2b4bb400fd64e0f393c7027d2e44d9f4e81
Varun Gollapalli <gvarun1999@gmail.com>
2023-09-27 17:27:24 -0700

    Time: 1 ms (81.28%), Space: 44.4 MB (26.33%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -2,19 +2,16 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         if(nums == null || nums.length == 0) return new int[]{};
         
-        ArrayList<Integer> even = new ArrayList<>();
-        ArrayList<Integer> odd = new ArrayList<>();
+        int[] temp = new int[nums.length];
         
-        for(int i:nums){
-            if(i%2==0) even.add(i);
-            else odd.add(i);
+        int j = 0, k = nums.length - 1;
+        
+        for(int i = 0;i<nums.length && j<nums.length && k>=0;i++)
+        {
+            if(nums[i]%2==0) temp[j++] = nums[i];
+            else temp[k--] = nums[i];
         }
         
-        int[] res = new int[nums.length];
-        int i = 0;
-        for(int j = 0;j<even.size();j++) res[i++] = even.get(j);
-        for(int j = 0;j<odd.size();j++) res[i++] = odd.get(j);
-        
-        return res;
+        return temp;
     }
 }
