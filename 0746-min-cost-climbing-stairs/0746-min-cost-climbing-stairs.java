6844a7c27dc706eb36e1db0eea6d403ce8c939ef
Varun Gollapalli <gvarun1999@gmail.com>
2023-10-13 16:25:49 -0700

    Time: 1 ms (46.18%), Space: 42.3 MB (99.31%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         if(cost == null || cost.length == 0) return 0;
-        int n = cost.length;
-        int[] dp = new int[n+1];
         
-        for(int i = 2;i<=n;i++)
+        int[] dp = new int[cost.length+1];
+        
+        for(int i = 2;i<=cost.length;i++)
         {
             int one = dp[i-1] + cost[i-1];
             int two = dp[i-2] + cost[i-2];
@@ -12,7 +12,6 @@ class Solution {
             dp[i] = Math.min(one,two);
         }
         
-        return dp[n];
-        
+        return dp[cost.length];
     }
 }
