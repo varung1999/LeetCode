ff4b787f4b4e3fba69b3b78efb838b7b875428a1
Varun Gollapalli <gvarun1999@gmail.com>
2024-01-02 09:40:05 -0500

    Time: 3 ms (77.12%), Space: 45.1 MB (9.75%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,40 +1,19 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int max = 1;
+        int[] freq = new int[nums.length + 1];
+        
         for(int i = 0;i<nums.length;i++)
         {
-            if(!map.containsKey(nums[i]))
+            if(freq[nums[i]]>=result.size())
             {
-                map.put(nums[i],1);
+                result.add(new ArrayList<>());
             }
-            else{
-                int val = map.get(nums[i]);
-                val++;
-                max = Math.max(max,val);
-                map.put(nums[i],val);
-            }
-        }
-        
-        for(int i = 0;i<max;i++)
-        {
-            result.add(new ArrayList<>()); 
-        }
-        
-        for(Map.Entry<Integer,Integer> entry: map.entrySet())
-        {
-            int currKey = entry.getKey();
-            int currVal = entry.getValue();
             
-            int i = 0;
-            while(currVal>0)
-            {
-                if(i == result.size()) i = 0;
-                result.get(i++).add(currKey);
-                currVal--;
-            }
+            result.get(freq[nums[i]]).add(nums[i]);
+            freq[nums[i]]++;
         }
         
         return result;
