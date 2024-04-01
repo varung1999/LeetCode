02fc2f198bb4cd9daead52a1bf39eb7a2f6345e9
Varun Gollapalli <gvarun1999@gmail.com>
2024-04-01 03:36:19 -0700

    Time: 1 ms (36.75%), Space: 41.6 MB (61.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         if(s == null || s.length() == 0) return 0;
-        
-        String[] splitArray = s.split(" ");
-        
-        return splitArray[splitArray.length-1].length();
+        String sArray[] = s.split(" ");
+        return sArray[sArray.length-1].length();
     }
 }
