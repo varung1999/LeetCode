0e83e986e68feff02daa9e6a405731e4fcd00fcf
Varun Gollapalli <gvarun1999@gmail.com>
2024-06-22 13:22:55 -0700

    Time: 28 ms (61.01%), Space: 94.7 MB (61.16%) - LeetHub

---
+++ 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long countCompleteDayPairs(int[] hours) {
+        if(hours == null || hours.length == 0) return 0;
+        
+        long count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int hour : hours){
+            int rem = hour%24;
+            
+            if(rem == 0){
+                count+= map.getOrDefault(0,0);
+            }
+            else{
+                count+=map.getOrDefault(24-rem,0);
+            }
+            
+            map.put(rem,map.getOrDefault(rem,0)+1);
+        }
+        
+        return count;
+    }
+}
