249c2c528b65652e0d4d77dda90ecfa211336146
Varun Gollapalli <gvarun1999@gmail.com>
2024-06-22 13:14:01 -0700

    Time: 3 ms (7.88%), Space: 42.3 MB (57.49%) - LeetHub

--- 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.java
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.java
@@ -4,17 +4,18 @@ class Solution {
         
         int count = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int hour: hours){
+        for(int hour: hours)
+        {
             int rem = hour%24;
             
             if(rem == 0){
                 count+= map.getOrDefault(0,0);
             }
             else{
-                count+= map.getOrDefault(24 - rem, 0);
+                count+= map.getOrDefault(24-rem,0);
             }
             
-            map.put(rem, map.getOrDefault(rem,0)+1);
+            map.put(rem,map.getOrDefault(rem,0)+1);
         }
         
         return count;
