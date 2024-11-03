993a0508fb953f2c8ba1c3a7deeba8011e7b5c04
Varun Gollapalli <gvarun1999@gmail.com>
2024-11-03 07:51:15 -0800

    Time: 0 ms (100.00%), Space: 41.3 MB (51.17%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s == null || s.length() == 0 || s.length()!=goal.length()) return false;
-        
-        StringBuilder sb = new StringBuilder(s);
-        sb.append(s);
-        
-        if(sb.toString().contains(goal)) return true;
-        return false;
+        return (s.length() == goal.length() && (s+s).contains(goal));
     }
 }
